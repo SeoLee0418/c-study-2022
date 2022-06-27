dbca4c3a454170d19cc41091c62ad0e06e445c31
SeoLee0418 <sksdltjdyd@snu.ac.kr>
2022-06-27 12:21:21 +0900

    C/C++ Study header-source

---
+++ header-source/header-source/main.cpp
@@ -0,0 +1,21 @@
+#include "header.h"
+
+int main(){
+    int a = pow(3,3);
+    srand(time(0));
+    int b = rand()% 6;
+    int c = sin(90);
+    int result = add();
+    
+    printf("%d\n", a);
+    printf("%d\n", b);
+    return 0;;
+}
+
+int add(){
+    int a;
+    int b;
+    int result = a + b;
+    
+    return result;
+}
---
+++ header-source/header.h
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+int add();
